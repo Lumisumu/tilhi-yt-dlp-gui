b24ce3d235a617bc7126a4febc0d42cdc7654229
Lumisumu <64976539+Lumisumu@users.noreply.github.com>
2024-03-27 18:20:16 +0200

    Squashed commit of the following:
    commit c44ba36d82ce81b0119ee4b71a234326033f5901
    Author: Lumisumu <64976539+Lumisumu@users.noreply.github.com>
    Date:   Mon Mar 25 18:43:10 2024 +0200
    
        Update readme with basic instructions
    
    commit b5660f5cf746155b696399c004ce3c58e0306c04
    Author: Lumisumu <64976539+Lumisumu@users.noreply.github.com>
    Date:   Mon Mar 25 18:39:03 2024 +0200
    
        Add option for audio track only
    
    commit 373e76c193f77be19457ff01ed5bfd710e6266fe
    Author: Lumisumu <64976539+Lumisumu@users.noreply.github.com>
    Date:   Sun Mar 24 19:24:06 2024 +0200
    
        Custom save location for clips
    
    commit 9dd81b5561017e5cd237889077030311c4dd135f
    Author: Lumisumu <64976539+Lumisumu@users.noreply.github.com>
    Date:   Sun Mar 24 16:51:12 2024 +0200
    
        Add option for custom save location, refactor, add notice about required field
    
    commit 40f7f7ea81b8d2143a450d4ec16617252992efa5
    Author: Lumisumu <64976539+Lumisumu@users.noreply.github.com>
    Date:   Sun Mar 24 13:48:24 2024 +0200
    
        Automatically edit the timestamps to usable format
    
    commit 3a86718e7ba795c1f20d49ad17766887fac258f8
    Author: Lumisumu <64976539+Lumisumu@users.noreply.github.com>
    Date:   Sun Mar 24 12:31:47 2024 +0200
    
        Modify multithreading, combine to one method, add clip naming
    
    commit 29b2a477cf366f2a0f69b9f9f3f7068286c41aea
    Author: Lumisumu <64976539+Lumisumu@users.noreply.github.com>
    Date:   Sat Mar 23 16:25:54 2024 +0200
    
        Apply icon
    
    commit 9f20940d9da3958fdc039903f07bc6f20ca8e3fb
    Author: Lumisumu <64976539+Lumisumu@users.noreply.github.com>
    Date:   Sat Mar 23 16:24:01 2024 +0200
    
        Add icon
    
    commit 423a5f9b14c211b1a796a45082e7aea43cab1d82
    Author: Lumisumu <64976539+Lumisumu@users.noreply.github.com>
    Date:   Sat Mar 23 16:10:20 2024 +0200
    
        Add multithreading to fix freezing
    
    commit ab0472523084b428ec39989b038bf6b1a74a632c
    Author: Lumisumu <64976539+Lumisumu@users.noreply.github.com>
    Date:   Sat Mar 23 13:15:31 2024 +0200
    
        Add update button and status text
    
    commit a47b9dc9d8c942e4783d2878f25f78380df89b6b
    Author: Lumisumu <64976539+Lumisumu@users.noreply.github.com>
    Date:   Fri Mar 22 11:56:54 2024 +0200
    
        Cover image is randomly chosen
    
    commit 6061fd17c4a412f5f716d941f40f8c1ea608fe13
    Author: Lumisumu <64976539+Lumisumu@users.noreply.github.com>
    Date:   Fri Mar 22 11:50:12 2024 +0200
    
        Edit image dimensions
    
    commit 084c92a7441443eb864d24e212d8cd01502adf14
    Author: Lumisumu <64976539+Lumisumu@users.noreply.github.com>
    Date:   Fri Mar 22 11:45:02 2024 +0200
    
        Rename images

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from PIL import Image, ImageTk
 import os
 import subprocess
 import string
+import random
+import threading as th
 
 def resize_image(event):
     global resized_tk
@@ -24,51 +26,118 @@ def resize_image(event):
     canvas.create_image(int(event.width / 2), int(event.height / 2), anchor="center", image=resized_tk)
 
 def start_download():
-    start_timestamp = start_hours_field.get() + ":" + start_minutes_field.get() + ":" + start_seconds_field.get()
-    end_timestamp = end_hours_field.get() + ":" + end_minutes_field.get() + ":" + end_seconds_field.get()
+    if url_field.get() != "":
 
-    if end_timestamp != "00:00:00":
-        download_and_clip(start_timestamp, end_timestamp)
+        # Set full video save location
+        if clip_folder_field.get() != "":
+            video_target_folder = clip_folder_field.get()
+        else:
+            video_target_folder = "full-videos"
 
-        if checkbox_keep_original.get() == "On":
-            file_name = subprocess.getoutput('yt-dlp --print filename ' + url_field.get())
-            file_name = file_name.replace("WARNING: [generic] Falling back on generic information extractor", "")
-            file_name = ''.join(file_name.splitlines())
-            os.remove(file_name)
+        # Set clip save location
+        if clip_folder_field.get() != "":
+            clip_target_folder = clip_folder_field.get()
+        else:
+            clip_target_folder = "clips"
+
+        # If folders do not exist yet, create them before downloading
+        if not os.path.exists(video_target_folder):
+            os.makedirs(video_target_folder)
+        if not os.path.exists(clip_target_folder):
+            os.makedirs(clip_target_folder)
+
+        # Create yt-dlp command to download the video
+        if checkbox_only_audio.get() == "On":
+            command = 'yt-dlp -P ' + video_target_folder + " -f 140 " + url_field.get()
+        else:
+            command = 'yt-dlp -P ' + video_target_folder + " " + url_field.get()
+
+        # Get video file name and modify it for file naming purpose
+        file_name = subprocess.getoutput('yt-dlp --print filename ' + url_field.get())
+        file_name = file_name.replace("WARNING: [generic] Falling back on generic information extractor", "")
+        file_name = ''.join(file_name.splitlines())
+
+        # Arrays for timestamp editing
+        timestamp_numbers = []
+        timestamp_numbers.extend([start_hours_field.get(), start_minutes_field.get(), start_seconds_field.get(), end_hours_field.get(), end_minutes_field.get(), end_seconds_field.get()])
+
+        # If end timestamp fields have any entries, make a clip. Otherwise only download.
+        if timestamp_numbers[3] != "" or timestamp_numbers[4] != "" or timestamp_numbers[5] != "":
+            edited_timestamps = []
+
+            for i in range(len(timestamp_numbers)):
+                if not timestamp_numbers[i]:
+                    # If the item is empty, replace it with "hello"
+                    timestamp_numbers[i] = "00"
+
+            # Handle timestamps to proper format
+            for s in timestamp_numbers:
+                # Trim to only two first numbers
+                if len(s) > 2:
+                    new_string = s[:2]
+                # Add missing number if only one number
+                elif len(s) == 1:
+                    new_string = '0' + s
+                else:
+                    new_string = s
+
+                edited_timestamps.append(new_string)
+
+            start_timestamp = edited_timestamps[0] + ":" + edited_timestamps[1] + ":" + edited_timestamps[2]
+            end_timestamp = edited_timestamps[3] + ":" + edited_timestamps[4] + ":" + edited_timestamps[5]
+
+            # Start download
+            subprocess.run(command, shell=True)
+
+            # Make a clip with FFmpeg, replace file extension if audio track only is selected
+            if checkbox_only_audio.get() == "On":
+                file_name = file_name[:-4]
+                file_name += "m4a"
+                cutter = 'ffmpeg -ss ' + start_timestamp + ' -to ' + end_timestamp + ' -i "' + video_target_folder + "/" + file_name + '" -c copy "' + clip_target_folder + '/' + 'clip - ' + file_name + '"'
+            else:
+                cutter = 'ffmpeg -ss ' + start_timestamp + ' -to ' + end_timestamp + ' -i "' + video_target_folder + "/" + file_name + '" -c copy "' + clip_target_folder + '/' + 'clip - ' + file_name + '"'
+            subprocess.run(cutter, shell=True)
+
+            if checkbox_keep_original.get() == "On":
+                os.remove(file_name)
+
+        else:
+            # Start download
+            subprocess.run(command, shell=True)
+
+        status_text.set("Download finished: " + file_name)
+
     else:
-        normal_download()
+        status_text.set("Error: url field is empty.")
 
-def normal_download():
-    # Download video with yt-dlp
-    command = 'yt-dlp ' + url_field.get()
-    subprocess.run(command, shell=True)
-
-def download_and_clip(start, end):
-    normal_download()
-
-    # Get video file name
-    file_name = subprocess.getoutput('yt-dlp --print filename ' + url_field.get())
-    file_name = file_name.replace("WARNING: [generic] Falling back on generic information extractor", "")
-    file_name = ''.join(file_name.splitlines())
-
-    # Cut video with ffmpeg
-    cutter = 'ffmpeg -ss ' + start + ' -to ' + end + ' -i "' + file_name + '" -c copy clip.webm'
-    subprocess.run(cutter, shell=True)
+def update_ytdlp():
+    subprocess.run("yt-dlp --update", shell=True)
+    status_text.set("yt-dlp updated.")
 
 # Create window, set size and window title
 window = tk.Tk()
 window.title("Tilhi - yt-dlp käyttöliittymä")
 window.geometry("950x600")
+window.iconbitmap("tilhi-icon.ico")
 
-# Image file
-image_original = Image.open("cover-images/cover3.jpg")
+# Choose one of the three cover images at random
+random_number = random.randint(1, 3)
+image_name = "cover-images/cover" + str(random_number) + ".jpg"
+image_original = Image.open(image_name)
 image_ratio = image_original.size[0] / image_original.size[1]
 image_tk = ImageTk.PhotoImage(image_original)
 
-# Storing variables
-checkbox_keep_original = tk.StringVar(value="Off")
+# Status text variable
+status_text = tk.StringVar()
+status_text.set("Input url and press Start to download.")
+
+# User selections
+video_target_folder = "full-videos"
+clip_target_folder = "clips"
 start_timestamp = "00:00:00"
 end_timestamp = "00:00:00"
+checkbox_keep_original = tk.StringVar(value="Off")
+checkbox_only_audio = tk.StringVar(value="Off")
 
 ### GRIDS ###
 
@@ -86,6 +155,7 @@ side_frame.rowconfigure(1, weight=1)
 side_frame.rowconfigure(2, weight=1)
 side_frame.rowconfigure(3, weight=1)
 side_frame.rowconfigure(4, weight=1)
+side_frame.rowconfigure(5, weight=1)
 
 # 1st content frame: file settings
 file_info_frame = tk.Frame(side_frame)
@@ -93,7 +163,11 @@ file_info_frame.grid(row=1, column=0, sticky="nsew")
 file_info_frame.columnconfigure(0, weight=1)
 file_info_frame.columnconfigure(1, weight=3)
 file_info_frame.columnconfigure(2, weight=1)
-file_info_frame.rowconfigure(0, weight=1)
+file_info_frame.rowconfigure(0, weight=3)
+file_info_frame.rowconfigure(1, weight=3)
+file_info_frame.rowconfigure(2, weight=3)
+file_info_frame.rowconfigure(3, weight=1)
+file_info_frame.rowconfigure(4, weight=1)
 
 # 2nd content frame: timestamps
 timestamps_frame = tk.Frame(side_frame)
@@ -109,6 +183,14 @@ timestamps_frame.rowconfigure(1, weight=1)
 timestamps_frame.rowconfigure(2, weight=1)
 timestamps_frame.rowconfigure(3, weight=1)
 
+# 3rd content frame: buttons
+buttons_frame = tk.Frame(side_frame)
+buttons_frame.grid(row=4, column=0, sticky="nsew")
+buttons_frame.columnconfigure(0, weight=1)
+buttons_frame.columnconfigure(1, weight=3)
+buttons_frame.columnconfigure(2, weight=1)
+buttons_frame.rowconfigure(0, weight=1)
+
 ### CONTENT ###
 
 # Content, left side: decoration image
@@ -122,8 +204,28 @@ url_label.grid(row=0, column=0, sticky="e")
 url_field = tk.Entry(file_info_frame)
 url_field.grid(row=0, column=1, sticky="ew", padx=20)
 
+# Content section 1: only audio
+only_audio_checkbutton = tk.Checkbutton(file_info_frame, text="Lataa vain ääniraita", font=('Arial', 13), variable=checkbox_only_audio, onvalue="On", offvalue="Off")
+only_audio_checkbutton.grid(row=1, column=1, sticky="w", padx=15)
+
+# Content section 1: full video folder selection field
+full_video_folder_label = tk.Label(file_info_frame, text="Tallennuskansio videoille:", font=('Arial', 15), height = 1)
+full_video_folder_label.grid(row=2, column=0, sticky="e")
+full_video_folder_field = tk.Entry(file_info_frame)
+full_video_folder_field.grid(row=2, column=1, sticky="ew", padx=20)
+
+# Content section 1: clip folder selection field
+clip_folder_label = tk.Label(file_info_frame, text="Tallennuskansio klipeille:", font=('Arial', 15), height = 1)
+clip_folder_label.grid(row=3, column=0, sticky="e")
+clip_folder_field = tk.Entry(file_info_frame)
+clip_folder_field.grid(row=3, column=1, sticky="ew", padx=20)
+
+# Content section 1: note about file locations
+folder_tips_label = tk.Label(file_info_frame, text="Jos tallennuskansioiden valinta jätetään tyhjäksi, uudet kansiot luodaan ohjelman kansioon.", font=('Arial', 11), height = 1)
+folder_tips_label.grid(row=4, column=1, sticky="w", padx=20)
+
 # Content section 2: clips text
-clips_label = tk.Label(timestamps_frame, text="Leikkaa klippi", font=('Arial', 15), height = 1)
+clips_label = tk.Label(timestamps_frame, text="Tee klippi", font=('Arial', 15), height = 1)
 clips_label.grid(row=0, column=0, sticky="e")
 
 # Clip section 2: clip start timestamp fields
@@ -158,9 +260,20 @@ end_seconds_field.grid(row=2, column=5, padx=20)
 keep_original_checkbutton = tk.Checkbutton(timestamps_frame, text="Poista alkuperäinen", font=('Arial', 13), variable=checkbox_keep_original, onvalue="On", offvalue="Off")
 keep_original_checkbutton.grid(row=3, column=1, sticky="ew", padx=15)
 
+# Button to update yt-dlp
+ytdlp_update_button = tk.Button(buttons_frame, text="Päivitä yt-dlp", font=('Arial', 13), command=update_ytdlp, height = 1, width = 13)
+ytdlp_update_button.grid(row=0, column=0, sticky="e", padx=30, pady=30)
+
+# Status text for guiding user
+status_label = tk.Label(buttons_frame, textvariable=status_text, font=('Arial', 13), wraplength=300, width=30, anchor="w")
+status_label.grid(row=0, column=1, sticky="w")
+
 # Button to start download
-start_button = tk.Button(side_frame, text="Aloita lataus", font=('Arial', 15), command=start_download, height = 1, width = 15)
-start_button.grid(row=4, column=0, sticky="e", padx=30, pady=30)
+start_button = tk.Button(buttons_frame, text="Aloita lataus", font=('Arial', 15), command=lambda: th.Thread(target=start_download).start(), height = 1, width = 15)
+start_button.grid(row=0, column=2, sticky="e", padx=30, pady=30)
+
+required_label = tk.Label(side_frame, text="* = Pakollinen kenttä", font=('Arial', 11), wraplength=300, width=30, anchor="w")
+required_label.grid(row=5, column=0, sticky="w", padx=20)
 
 # Start process
 window.mainloop()
